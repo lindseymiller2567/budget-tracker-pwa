ba19e127db92b4079ad0475e8415d391d6081f51
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-04-11 21:24:22 -0500

    cleaned up comments in file

--- public/service-worker.js
+++ public/service-worker.js
@@ -88,16 +88,5 @@ self.addEventListener("fetch", function (evt) {
                 }
             })
         }));
-    // fetch(evt.request).catch(function () {
-    //     return caches.match(evt.request).then(function (response) {
-    //         if (response) {
-    //             console.log("if response")
-    //             return response;
-    //         } else if (evt.request.headers.get('accept').includes('text/html')) {
-    //             console.log("else if response")
-    //             return caches.match('/').then(response => response);
-    //         }
-    //     });
-    // })
 }
 )
